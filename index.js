b71187a522679e36f4e934e0667ca75ea639ca3d
Kunal <kunal20vijay00@gmail.com>
2023-12-02 02:51:32 +0530

    emptycup backend

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Loading the environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB (replace 'your-mongodb-uri' with your actual MongoDB connection URI)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+// Routes
+const designersRouter = require('./routes/designer'); 
+app.use('/', designersRouter);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
